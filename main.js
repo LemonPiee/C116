977ab39169230e757b35ddb56921af5f90f84162
LemonPiee <107181480+LemonPiee@users.noreply.github.com>
2023-04-17 09:52:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function startClassification{
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/vixSnXfwG/model.json',modelready);
+}
+
+function modelready(){
+    classifier.classify(gotResults);
+    
+}
+
+function gotResults(error,results){
+    console.log(results);
+}
